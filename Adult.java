6fbf5ea3915b23f17f9dedf8c0c0fd18dfdc6d30
Natia94 <natiakhoma@gmail.com>
2017-06-20 13:56:20 +0400

    commit Adult class

---
+++ Adult.java
@@ -0,0 +1,32 @@
+
+public class Adult extends Costumer {
+    private  final double SAVINGS_INTEREST  = 5;
+    private  final double CHECKING_INTEREST  = 6;
+    private  final double CHECK_CHARGE  = 7;
+    private  final double OVERDRAFT_PENALTY  = 8;
+
+    public Adult(String s){
+        this.setCustomerNumber(s);
+    }
+
+
+
+
+
+
+
+    @Override
+    public double getCheckingInterest() {
+        return this.CHECKING_INTEREST;
+    }
+
+    @Override
+    public double getCheckCharge() {
+        return this.CHECK_CHARGE;
+    }
+
+    @Override
+    public double getSavingsInterest() {
+        return this.SAVINGS_INTEREST;
+    }
+}
