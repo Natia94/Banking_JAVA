871694dfb1b2df9f877ccf9583d93906874284dd
Natia94 <natiakhoma@gmail.com>
2017-06-20 13:57:34 +0400

    commit SavingAccount class

---
+++ SavingsAccount.java
@@ -0,0 +1,16 @@
+
+public class SavingsAccount extends Acount {
+    public SavingsAccount(Costumer costumer) {
+        super(costumer);
+    }
+
+    public void deposit(double value) {
+
+    }
+    public void withdrow(double value){
+
+    }
+    public void addInterest(double value){
+
+    }
+}
